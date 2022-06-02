317209347b5a8558e9a111a8c79836044b93ee0a
Huda Al Zahabi <hudazahabi00@gmail.com>
2022-06-02 18:25:02 +0300

    Added comments and sort descending by date

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function start() {
+    //linking each button with its functionality
     const addBtn = $("#add_btn");
     addBtn.click(addItem);
 
@@ -8,14 +9,18 @@ function start() {
     const removeBtn = $("#remove_btn");
     removeBtn.click(removeItem);
 }
-let id = 0;
 
 function addItem() {
+    //take the input values from the user and generate an id
+    let id = Math.floor(Math.random() * 1000) + 1;
     let title = $("#add_title").val();
     let description = $("#add_description").val();
     let points = $("#add_points").val();
-    id++;
+    let d = new Date();
+    let timestamp =
+        d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
 
+    //save them to the todo list table
     $("#todo_table tbody").append(
         "<tr>" +
         "<td>" +
@@ -33,20 +38,29 @@ function addItem() {
         "<td>" +
         false +
         "</td>" +
-        "<td>33</td>"
+        "<td>" +
+        timestamp +
+        "</td>"
     );
+    //sort descending according to date
+    $("tr")
+        .sort(function(a, b) {
+            return (
+                new Date($(a).find("input").val()).getTime() <
+                new Date($(b).find("input").val()).getTime()
+            );
+        })
+        .appendTo("tbody");
 }
 
 function updateItem() {
+    //take the input values from the user
     let id = $("#update_id").val();
     let title = $("#update_title").val();
     let description = $("#update_description").val();
     let points = $("#update_points").val();
+    //if there is any input, ie the user wants to change a certain field, update it
     if (title) {
-        $("#" + id)
-            .parents("tr")
-            .find("td:eq(0)")
-            .html(title);
         $("#todo_table")
             .find("tr#" + id)
             .find("td:eq(1)")
@@ -67,6 +81,7 @@ function updateItem() {
 }
 
 function removeItem() {
+    //take from the user the id of the row he wants to delete and remove it from table
     let idInput = $("#remove_id").val();
     let rowintable = $("#" + idInput);
     rowintable.closest("tr").remove();
